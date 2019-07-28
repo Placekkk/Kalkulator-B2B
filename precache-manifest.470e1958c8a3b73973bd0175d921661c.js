4e04d76840c5d28a29f0fa83c7d285ff67d7e749
Lukasz Huras <hurasek2@o2.pl>
2019-07-28 20:27:11 +0200

    Updates

---
+++ precache-manifest.470e1958c8a3b73973bd0175d921661c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f717b4a98b198676f3cea2fec4f9f88",
+    "url": "/Kalkulator-B2B/index.html"
+  },
+  {
+    "revision": "de1d915b4b6a009a2e1e",
+    "url": "/Kalkulator-B2B/static/css/main.39d2f28c.chunk.css"
+  },
+  {
+    "revision": "43842c2c96165b0bc3b0",
+    "url": "/Kalkulator-B2B/static/js/2.dd08f88c.chunk.js"
+  },
+  {
+    "revision": "de1d915b4b6a009a2e1e",
+    "url": "/Kalkulator-B2B/static/js/main.f0165fca.chunk.js"
+  },
+  {
+    "revision": "6d9e3d279d70d4e36457",
+    "url": "/Kalkulator-B2B/static/js/runtime~main.cafe6a4e.js"
+  }
+]);
