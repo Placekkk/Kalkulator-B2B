6cd60d347414de9df1ffa77476ec5d3c4f23eacf
Lukasz Huras <hurasek2@o2.pl>
2019-07-28 20:50:07 +0200

    probably fixed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -32,12 +32,6 @@ class App extends React.Component {
                 </form>
             </div>
 
-            <div className={'test2'}>
-                <p>first result is <span className={'money-display'}>000</span></p>
-                <p>second result is <span className={'money-display'}>000</span></p>
-            </div>
-
-            </div>
 
         </div>
 
