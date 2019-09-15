7df243e1b8f5e1103f116460792a1557c812c06a
Lukasz Huras <hurasek2@o2.pl>
2019-09-15 10:00:13 +0200

    please be over v2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -56,7 +56,7 @@ class App extends React.Component {
         <div>
             <Provider store={store}>
             {this.state.shouldRender? <MainCalc testFunction={this.tryRefresh} showButtonDisplay={this.handleShowButtonDisplay}/> : null}
-            <h2 className={'show-advanced-calc-trigger'} onClick={this.handleShowHide}>{this.state.foldText} {this.state.foldText === 'Pokaż szczegółowe wyliczenia' ? "▼" : "▲"}</h2>
+            <h2 style={this.state.showButtonStyle} className={'show-advanced-calc-trigger'} onClick={this.handleShowHide}>{this.state.foldText} {this.state.foldText === 'Pokaż szczegółowe wyliczenia' ? "▼" : "▲"}</h2>
           <AdvancedValuesHolder handleUpdate={this.handleUpdate} advancedCalcShowHideStyle={this.state.advancedCalcStyle}/>
         </Provider>
         </div>
